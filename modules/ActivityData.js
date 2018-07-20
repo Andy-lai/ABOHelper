0c361095b83902a203a074f0602828eb48822829
MonarchChakri <chakradhar51.p@gmail.com>
2018-07-20 23:27:14 +0530

    Activity Data

---
+++ modules/ActivityData.js
@@ -0,0 +1,55 @@
+//Type your code here
+var ActivityData = [
+  {
+    "Today": [
+      {
+        "text": "Call Murphy Clemons",
+        "userId": [
+          0
+        ]
+      },
+      {
+        "text": "Blevins Shaffer purchased Artistry Youth Extend 1 month ago today.",
+        "userId": [
+          2
+        ]
+      }
+    ]
+  },
+  {
+    "Tomorrow": [
+      {
+        "text": "Time for a sports discount!",
+        "userId": [
+          6,
+          8
+        ]
+      },
+      {
+        "text": "Message 4 newly added interested contacts",
+        "userId": [
+          3,
+          4,
+          5,
+          6
+        ]
+      }
+    ]
+  },
+  {
+    "Upcoming": [
+      {
+        "text": "Emma Martinez purchased Satinique Scalp Tonic 3 months ago today.",
+        "userId": [
+          5
+        ]
+      },
+      {
+        "text": "Follow up Blevins Shaffer (Artistry Youth Extend)",
+        "userId": [
+          2
+        ]
+      }
+    ]
+  }
+]
