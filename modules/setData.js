910c6d26c8620cacb57a437cfd7e33fef90b0226
Uttej <uttej60@gmail.com>
2018-07-20 16:04:36 +0530

    commit frmSearch

--- modules/setData.js
+++ modules/setData.js
@@ -1,20 +1,38 @@
 function searchOnClick() {
 	var users = Users;
+  	var groups = Groups;
+    var tags = Tags;
+  	var products = Products;
+  
+  	var categories = {'Users':users,'Tags':tags,'Groups':groups,'Products':products};
   
   	data = [
- [
-   {lblSearchHeader:"Users"},
-             []
- ],
-];
+ 		
+	];
   
-  for(i = 0; i < 9; i++){
-    firstName = users[i].firstName;
-    if(firstName != null && frmSearch.searchInput != null && frmSearch.searchInput.text != null)
-    if(firstName.toLowerCase().indexOf(frmSearch.searchInput.text.toLowerCase()) > 0){
-    	data[0][1].push({ lblSearchContent:  firstName});
-    }
+  var i = 0;
+  
+	for(var category in categories){
+      data.push([
+   			{lblSearchHeader:category},
+   			[]
+ 		]);
+    	for(var item in category){
+    		firstName = item.firstName;
+    		if(firstName != null && frmSearch.searchInput != null && frmSearch.searchInput.text != null){
+    		if(firstName.toLowerCase().indexOf(frmSearch.searchInput.text.toLowerCase()) > 0){
+    			data[i][1].push({ lblSearchContent:  firstName});
+    		}
+            tag = item.tag;
+    		if(tag != null && frmSearch.searchInput != null && frmSearch.searchInput.text != null){
+    		if(tag.toLowerCase().indexOf(frmSearch.searchInput.text.toLowerCase()) > 0){
+    			data[i][1].push({ lblSearchContent:  tag});
+    		}
+        }
+      }
   }
+      i++;
+    }
   
  	frmSearch.sgmtSearchResults.setData(data);
 }
