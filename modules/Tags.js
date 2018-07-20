3be4d17c6b528db4215b1626e33109f43c0d123c
Uttej <uttej60@gmail.com>
2018-07-20 14:10:44 +0530

    Mock Data

---
+++ modules/Tags.js
@@ -0,0 +1,43 @@
+// Tags Model
+var Tags = [{
+		"tag": "beauty",
+		"users": [
+			6,
+			8
+		]
+	},
+	{
+		"tag": "home",
+		"users": [
+			1
+		]
+	},
+	{
+		"tag": "sports",
+		"users": [
+			7
+		]
+	},
+	{
+		"tag": "nutrition",
+		"users": [
+			1,
+			7,
+			9
+		]
+	},
+	{
+		"tag": "patience",
+		"users": [
+			1,
+			5
+
+		]
+	},
+	{
+		"tag": "avoid",
+		"users": [
+
+		]
+	}
+]
